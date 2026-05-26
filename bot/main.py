c77ef0e18c72d881e3cd284e98f120c5a0e83890
Brentley Bozeman <bozemanbrentley659@gmail.com>
2026-05-26 10:54:49 -0500

    feat: initialize Discord bot client

--- bot/main.py
+++ bot/main.py
@@ -1,7 +1,21 @@
 # Required libraries for Discord bot functionality and async operations, load secret credentials.
+
 import discord 
 import asyncio
 import os 
 
 from discord.ext import commands
 from dotenv import load_dotenv
+load_dotenv()
+
+class Client(discord.Client):
+    async def on_ready(self):
+        print(f'\u2705 Successfully logined as {self.user}')
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+Discord_bot_token = os.getenv("DISCORD_TOKEN")
+
+client = Client(intents=intents)
+client.run(Discord_bot_token)
