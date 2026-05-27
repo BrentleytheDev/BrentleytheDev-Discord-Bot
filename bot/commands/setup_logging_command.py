d004ee7d1c703fb65f281608bd75123a73fc1e30
Brentley Bozeman <bozemanbrentley659@gmail.com>
2026-05-26 23:29:24 -0500

    feat: add command to setup log channel for logging

--- bot/commands/setup_logging_command.py
+++ bot/commands/setup_logging_command.py
@@ -5,3 +5,29 @@ import json
 
 from discord.ext import commands 
 from discord import app_commands
+from pathlib import Path
+
+CONFIG_PATH = Path(__file__).resolve().parent.parent / "config" / "automod" / "config.json"
+
+async def setup(bot):
+        await bot.add_cog(Logging(bot))
+
+class Logging(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+
+    @app_commands.command(name="logging_channel", description="Set the general logging channel")
+    async def logging_channel(self, interaction: discord.Interaction, channel: discord.TextChannel):
+        try:
+            with open(CONFIG_PATH, 'r') as f:
+                data = json.load(f)
+                
+                if "logging" not in data:
+                    data["logging"] = {}
+                data["logging"]["general-server-logging"] = channel.id
+
+                with CONFIG_PATH.open("w", encoding="utf-8") as f:
+                    json.dump(data, f, indent=2)
+        except:
+             print("Failed :(")
